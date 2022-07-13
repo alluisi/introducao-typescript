e87a0436ba7e2d22614d7c1db57c5334e92a325b
alluisi <lecaluisi@hotmail.com>
2022-07-13 19:59:44 -0300

    iferencia e annotation

--- dist/js/index.js
+++ dist/js/index.js
@@ -2,5 +2,9 @@
 // string, boolean, number...
 let x = 10;
 // x = "teste";
-x = 16;
+x = 12;
 console.log(x);
+// inferência X annotation
+let y = 12; // inferência
+// y = "teste";
+let z = 12; // anotation
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 // string, boolean, number...
 let x: number = 10;
-
 // x = "teste";
-x = 16;
+
+x = 12;
 
 console.log(x);
+
+// inferência X annotation
+let y = 12; // inferência
+// y = "teste";
+
+let z: number = 12; // anotation
