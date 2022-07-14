93805c145d1242ced87fd5a21cd6a0fdcc10c907
alluisi <lecaluisi@hotmail.com>
2022-07-14 14:45:57 -0300

    type alias

--- src/index.ts
+++ src/index.ts
@@ -50,3 +50,9 @@ let id: string | number = "10";
 id = 200;
 // id = true;
 // id = [];
+
+// type alias
+type myIdType = number | string;
+const userId: myIdType = 10;
+const productId: myIdType = "00001";
+const shirId: myIdType = 123;
